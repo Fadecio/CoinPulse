9f6ded1a082820989e7243bfefcbaf86623f5602
unknown <fadeciolemos@gmail.com>
2026-05-26 16:37:09 -0300

    feat: adiciona renderização dinâmica, filtro de busca, loading, atualização automática e alternância de tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,153 @@
-const buscarBtn = document.getElementById('search-button');
-const atualizarBtn = document.getElementById('refresh-button');
-const currencyGrid = document.getElementById('currency-grid');
-const moedas = "USD-BRL, EUR-BRL, BCT-BRL";
+const themeButton = document.getElementById("theme-button");
 
-async function buscarMoedas(){
-    const url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL";
+const currencyGrid = document.getElementById("currency-grid");
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        renderizarMoedas(data);
-    } catch (error) {
-        console.error("Erro ao buscar moedas:", error);
+const buscarBtn = document.getElementById("search-button");
+const atualizarBtn = document.getElementById("refresh-button");
+
+const searchInput = document.getElementById("search-input");
+let moedasRenderizadas = [];
+
+const moedas = ["USD-BRL", "EUR-BRL", "BTC-BRL", "GBP-BRL", "ARS-BRL"];
+
+const API_URL = `https://economia.awesomeapi.com.br/json/last/${moedas.join(",")}`;
+
+async function buscarMoedas() {
+  try {
+    mostrarLoading();
+
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar moedas");
+    }
+
+    const data = await response.json();
+    moedasRenderizadas = Object.values(data);
+
+    renderizarMoedas(moedasRenderizadas);
+  } catch (error) {
+    mostrarErro();
+    console.error(error);
+  }
+}
+
+function pesquisarMoedas() {
+  const termo = searchInput.value.toLowerCase();
+
+  const moedasFiltradas = moedasRenderizadas.filter((moeda) => {
+    return (
+      moeda.code.toLowerCase().includes(termo) ||
+      moeda.name.toLowerCase().includes(termo)
+    );
+  });
+
+  renderizarMoedas(moedasFiltradas);
+}
+
+function renderizarMoedas(moedas) {
+  currencyGrid.innerHTML = "";
+  moedas.forEach((moeda) => {
+    const variacao = Number(moeda.pctChange);
+
+    const statusClass = variacao >= 0 ? "positive" : "negative";
+
+    const simbolo = variacao >= 0 ? "+" : "";
+
+    const precoFormatado = Number(moeda.bid).toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    const card = `<article class="currency-card">
+                        <div class="currency-top">
+                            <div>
+                                <span class="currency-symbol">
+                                ${moeda.code}
+                                </span>
+                                
+                                <h4>
+                                ${moeda.name}
+                                </h4>
+                            </div>
+
+                            <span class="status ${statusClass}">
+                                ${simbolo}${variacao.toFixed(2)}%
+                            </span>
+                        </div>
+
+                        <div class="currency-price">
+                            ${precoFormatado}
+                        </div>
+                        <div class="currency-footer">
+                            <span>
+                                Atualizado agora
+                            </span>
+                        </div>
+                    </article>
+                `;
+
+    currencyGrid.innerHTML += card;
+  });
+}
+
+function mostrarLoading() {
+  currencyGrid.innerHTML = `
+        <article class="currency-card">
+            <div class="loading"></div>
+        </article>
+
+        <article class="currency-card">
+            <div class="loading"></div>
+        </article>
+
+        <article class="currency-card">
+            <div class="loading"></div>
+        </article>        
+    `;
+}
+
+function mostrarErro() {
+  currencyGrid.innerHTML = `        
+            <div class="error">
+                Erro ao carregar dados.
+            </div>
+        
+    `;
+}
+
+function alternarTema(){
+    document.documentElement.classList.toggle("light-mode");
+    const temaClaroAtivo = document.documentElement.classList.contains("light-mode");
+
+    localStorage.setItem(
+        "theme",
+        temaClaroAtivo ? "light" : "dark"
+    );
+
+    atualizarTextoTema(temaClaroAtivo);
+}
+
+function carregarTema(){
+    const temaSalvo = localStorage.getItem("theme");
+
+    if (temaSalvo === "ligth"){
+        document.documentElement.classList.add("light-mode");
+
+        atualizarTextoTema(true);
     }
 }
 
-function renderizarMoedas(data) {
-
+function atualizarTextoTema(ligthMode) {
+    themeButton.textContent = 
+    ligthMode ? "Ligth Mode" : "Dark Mode";
 }
 
+atualizarBtn.addEventListener("click", buscarMoedas);
 buscarMoedas();
+
+themeButton.addEventListener("click", alternarTema);
+carregarTema();
+
+setInterval(buscarMoedas, 30000);
+
+searchInput.addEventListener("input", pesquisarMoedas);
